d262d2ba154d7b0c3f2dc43f73973388a03dc308
JISHNU AJI <jishnumaster16@gmail.com>
2026-05-17 00:42:15 +0530

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-if [ $# -lt 3 ]; then
-  echo "Usage: $0 <IP> <start_port> <end_port>"
-  exit 1
-fi
 
-ip=$1
-start_port=$2
-end_port=$3
+TARGET=$1
+START_PORT=1
+END_PORT=1024
 
-scan_ports() {
-  for ((port=$start_port; port<=$end_port; port++)); do
-    (echo > /dev/tcp/$ip/$port) &>/dev/null && echo "Port $port is open"
-  done
-}
+echo "Scanning $TARGET..."
 
-echo "Scanning $ip from port $start_port to $end_port..."
-scan_ports
-
+for ((port=$START_PORT; port<=$END_PORT; port++))
+do
+    timeout 1 bash -c "echo >/dev/tcp/$TARGET/$port" 2>/dev/null &&
+        echo "Port $port is OPEN"
+done
